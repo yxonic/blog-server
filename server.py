060e5506207eb4cd630200200d093d9cabda7b7c
凉风 <yinyu3@xiaohongshu.com>
2024-10-26 23:33:26 +0800

    solution for routing

--- server.py
+++ server.py
@@ -1,20 +1,67 @@
+import mimetypes
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
+        code = 200
 
-        self.send_header('Content-type', 'text/html')
+        # routing
+        if self.path.startswith('/public/'):
+            try:
+                f = open('.' + self.path, 'rb')
+                self.send_response(200)
+                content_type, _ = mimetypes.guess_type(self.path)
+                self.send_header('Content-Type', content_type)
+                self.end_headers()
+                self.wfile.write(f.read())
+                return
+            except:
+                code = 404
+                title = '404 not found'
+                content = '<h1>404 not found</h1>'
+        elif self.path == '/':
+            title = 'Home'
+            content = '<h1>Home</h1><p>Welcome to my website!</p>'
+        elif self.path == '/blogs':
+            title = 'Blogs'
+            blog_list = [f'<li><a href="{url}">{title}</a></li>' for title, url in [
+                ('blog 1', '/blog/1'),
+                ('blog 2', '/blog/2'),
+                ('blog 3', '/blog/3'),
+            ]]
+            content = f'<h1>Blogs</h1><ul>{"".join(blog_list)}</ul>'
+        elif self.path.startswith('/blog/'):
+            blog_id = int(self.path.lstrip('/blog/'))
+            title = f'Blog {blog_id}'
+            content = f'<h1>Blog {blog_id}</h1><p>dummy blog content</p>'
+        else:
+            code = 404
+            title = '404 Not Found'
+            content = '<h1>404 Not Found</h1>'
+
+        # rendering
+        template = open('templates/index.html').read()
+        nav = self.make_nav()
+        page = template \
+            .replace('{{title}}', title) \
+            .replace('{{content}}', content) \
+            .replace('{{nav}}', nav)
+
+        self.send_response(code)
+        self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        
-        data = f'''<html>
-            <body>
-                <p>Request path: <code>{self.path}</code></p>
-                <p>Request headers: <pre>{self.headers}</pre></p>
-            </body>
-        </html>'''
-        self.wfile.write(data.encode('utf-8'))
+        self.wfile.write(page.encode('utf-8'))
+
+    def make_nav(self):
+        items = [
+            ('Home', '/'),
+            ('Blogs', '/blogs'),
+        ]
+        return ''.join(
+            f'<li class="{"active" if self.path == href else ""}"><a href="{href}">{text}</a></li>'
+            for text, href in items
+        )
 
 
 if __name__ == '__main__':
